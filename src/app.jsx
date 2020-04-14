b9ed4fe57dff052208c939c2fe4bc282051759ff
ionware <ionwarez@gmail.com>
2020-04-14 02:29:59 +0100

    setup postcss and tailwindcss

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 
 const App = () => {
-  return <h1>Hello World</h1>;
+  return <h1 className='text-5xl text-center text-blue-600'>Quick Feed</h1>;
 };
 
 render(<App />, document.getElementById('app'));
